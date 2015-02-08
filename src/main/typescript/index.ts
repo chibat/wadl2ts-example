0d895e60ceab18e99724a05e648c362ea974e043
t1000leaf <tomofumi.chiba@gmail.com>
2015-02-09 08:20:59 +0900

    -

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,12 +1,14 @@
 /// <reference path="client.ts" />
 
-$('#form').submit(()=>{
-  var arg1 = $('#arg1').val();
-  var arg2 = $('#arg2').val();
-  client.calculator.add.get(arg1, arg2, (result)=>{
-    $('#result').text(result.value);
+$(function(){
+  $('#form').submit(()=>{
+    var arg1 = $('#arg1').val();
+    var arg2 = $('#arg2').val();
+    client.calculator.add.get(arg1, arg2, (result)=>{
+      $('#result').text(result.value);
+    });
+    return false;
   });
-  return false;
 });
 
 
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,8 +1,10 @@
-$('#form').submit(function () {
-    var arg1 = $('#arg1').val();
-    var arg2 = $('#arg2').val();
-    client.calculator.add.get(arg1, arg2, function (result) {
-        $('#result').text(result.value);
+$(function () {
+    $('#form').submit(function () {
+        var arg1 = $('#arg1').val();
+        var arg2 = $('#arg2').val();
+        client.calculator.add.get(arg1, arg2, function (result) {
+            $('#result').text(result.value);
+        });
+        return false;
     });
-    return false;
 });
